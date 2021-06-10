366582a7d51a13e9d2bf2e0b8e6041d2fdde3c07
Christian Maniewski <code@chmanie.com>
2021-06-10 15:00:54 +0200

    Handle simultaneous button presses

--- firmware/src/drivers/push_buttons.rs
+++ firmware/src/drivers/push_buttons.rs
@@ -5,8 +5,6 @@ use stm32f4xx_hal::gpio::{
     Input, PullUp,
 };
 
-use micromath::F32Ext;
-
 type PinA = PC0<Input<PullUp>>;
 type PinB = PC1<Input<PullUp>>;
 type PinC = PC2<Input<PullUp>>;
@@ -24,71 +22,89 @@ pub struct PushButtons {
     button_b: Button<PinB>,
     button_c: Button<PinC>,
     button_d: Button<PinD>,
-    state: Queue<[ButtonState; 4], 2>,
+    buffer: Queue<[ButtonState; 4], 2>,
+    counter: u16,
+    debounced: u8,
+    press_threshold: u16,
+    long_press_threshold: u16,
 }
 
 impl PushButtons {
     // In seconds
-    const LONG_PRESS_THRESHOLD: u8 = 2;
+    // PRESS_THRESHOLD is the window in which simultaneous button presses are recognized
+    const PRESS_THRESHOLD: f32 = 0.01;
+    const LONG_PRESS_THRESHOLD: f32 = 2.0;
 
     pub fn new(pin_a: PinA, pin_b: PinB, pin_c: PinC, pin_d: PinD, f_refresh: u16) -> Self {
-        // threshold = x (s) / (sample_size * f_refresh (Hz))
-        let long_press_threshold =
-            (Self::LONG_PRESS_THRESHOLD as f32 / (8.0 / f_refresh as f32)).round() as u16;
         Self {
-            button_a: Button::new(pin_a, long_press_threshold),
-            button_b: Button::new(pin_b, long_press_threshold),
-            button_c: Button::new(pin_c, long_press_threshold),
-            button_d: Button::new(pin_d, long_press_threshold),
-            state: Queue::new(),
+            button_a: Button::new(pin_a),
+            button_b: Button::new(pin_b),
+            button_c: Button::new(pin_c),
+            button_d: Button::new(pin_d),
+            buffer: Queue::new(),
+            counter: 0,
+            debounced: 0,
+            press_threshold: (f_refresh as f32 / (1.0 / Self::PRESS_THRESHOLD)) as u16,
+            long_press_threshold: (f_refresh as f32 / (1.0 / Self::LONG_PRESS_THRESHOLD)) as u16,
         }
     }
 
     pub fn read(&mut self) -> [ButtonState; 4] {
-        self.state.dequeue().unwrap_or([ButtonState::Idle; 4])
+        self.buffer.dequeue().unwrap_or([ButtonState::Idle; 4])
     }
 
-    pub fn read_raw(&mut self) -> [u8; 4] {
-        [
-            self.button_a.raw_state(),
-            self.button_b.raw_state(),
-            self.button_c.raw_state(),
-            self.button_d.raw_state(),
-        ]
+    pub fn poll(&mut self) {
+        let state = (self.button_a.raw_state() << 3)
+            | (self.button_b.raw_state() << 2)
+            | (self.button_c.raw_state() << 1)
+            | self.button_d.raw_state();
+        self.debounced |= state;
+        // Any button was pressed at least once
+        if self.debounced > 0 {
+            self.counter = self.counter.wrapping_add(1);
+            // Button was let go of
+            if state == 0 {
+                if self.counter >= self.long_press_threshold {
+                    return self.set_pressed(ButtonState::LongPress);
+                } else if self.counter >= self.press_threshold {
+                    return self.set_pressed(ButtonState::Press);
+                }
+            }
+        }
     }
 
-    pub fn poll(&mut self) -> () {
-        self.state
+    fn set_pressed(&mut self, button_state: ButtonState) {
+        self.buffer
             .enqueue([
-                self.button_a.poll(),
-                self.button_b.poll(),
-                self.button_c.poll(),
-                self.button_d.poll(),
+                self.from_bits(3, button_state),
+                self.from_bits(2, button_state),
+                self.from_bits(1, button_state),
+                self.from_bits(0, button_state),
             ])
             .ok();
+        self.debounced = 0;
+        self.counter = 0;
+    }
+
+    fn from_bits(&self, idx: usize, button_state: ButtonState) -> ButtonState {
+        if self.debounced >> idx & 1 > 0 {
+            button_state
+        } else {
+            ButtonState::Idle
+        }
     }
 }
 
-pub struct Button<PIN> {
+struct Button<PIN> {
     pin: PIN,
-    pub sample_idx: u8,
-    pub samples: u8,
-    pub long_press_counter: u16,
-    pub long_press_threshold: u16,
 }
 
 impl<PIN> Button<PIN>
 where
     PIN: InputPin,
 {
-    pub fn new(pin: PIN, long_press_threshold: u16) -> Self {
-        Self {
-            pin,
-            sample_idx: 0,
-            samples: 0,
-            long_press_counter: 0,
-            long_press_threshold,
-        }
+    pub fn new(pin: PIN) -> Self {
+        Self { pin }
     }
 
     fn raw_state(&self) -> u8 {
@@ -101,33 +117,5 @@ where
             }
         }
         0
-    }
-
-    pub fn poll(&mut self) -> ButtonState {
-        // Read one byte of samples
-        if self.sample_idx == 7 {
-            self.sample_idx = 0;
-            if self.samples > 0 {
-                // The button was pressed, increment long press counter
-                if self.long_press_counter < u16::MAX {
-                    self.long_press_counter += 1;
-                }
-            } else if self.long_press_counter > 0 {
-                // Button was let go of but long press counter had been started
-                if self.long_press_counter > self.long_press_threshold {
-                    // It's a long press!
-                    self.long_press_counter = 0;
-                    return ButtonState::LongPress;
-                }
-                // Long press counter started but we don't have presses anymore. It's a short press
-                self.long_press_counter = 0;
-                return ButtonState::Press;
-            }
-            self.samples = 0;
-        }
-        // Collect samples and prevent overflow
-        self.samples |= self.raw_state() << self.sample_idx;
-        self.sample_idx += 1;
-        ButtonState::Idle
     }
 }
